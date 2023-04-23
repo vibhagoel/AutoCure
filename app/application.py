38890e0848156eccba01d1230d3999c3bd8d70f0
Vibha Goel <vibhagoelbe@gmail.com>
2023-04-22 20:50:37 -0400

    Initial Structure

---
+++ app/application.py
@@ -0,0 +1,16 @@
+# from pages.main_page import MainPage
+# from pages.header import Header
+# from pages.search_results_page import SearchResultsPage
+# from pages.sign_in_page import SignInPage
+# from pages.cart_page import CartPage
+#
+#
+# class Application:
+#
+#     def __init__(self, driver):
+#         self.driver = driver
+#         self.main_page = MainPage(self.driver)
+#         self.header = Header(self.driver)
+#         self.search_results_page = SearchResultsPage(self.driver)
+#         self.sign_in_page = SignInPage(self.driver)
+#         self.cart_page = CartPage(self.driver)
---
+++ features/environment.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from app.application import Application
+
+
+def browser_init(context):
+    """
+    :param context: Behave context
+    """
+    context.driver = webdriver.Chrome(executable_path=r"C:\Users\shekh\OneDrive\Desktop\Vibha\AutoCure\chromedriver_win32\chromedriver.exe")
+
+    # context.browser = webdriver.Safari()
+    # context.browser = webdriver.Firefox()
+
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(4)
+    context.driver.wait = WebDriverWait(context.driver, 10)
+    context.app = Application(driver=context.driver)
+
+
+def before_scenario(context, scenario):
+    print('\nStarted scenario: ', scenario.name)
+    browser_init(context)
+
+
+def before_step(context, step):
+    print('\nStarted step: ', step)
+
+
+def after_step(context, step):
+    if step.status == 'failed':
+        print('\nStep failed: ', step)
+
+
+def after_scenario(context, feature):
+    context.driver.delete_all_cookies()
+    context.driver.quit()
