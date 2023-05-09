995c9dc6af2d7ed8e92fd228dd713651b4d14980
Vibha Goel <vibhagoelbe@gmail.com>
2023-05-08 23:24:16 -0400

    allure added

--- features/environment.py
+++ features/environment.py
@@ -1,3 +1,5 @@
+import allure
+from allure_commons.types import AttachmentType
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 # from selenium.webdriver.firefox.service import Service
@@ -5,13 +7,14 @@ from selenium.webdriver.support.wait import WebDriverWait
 from app.application import Application
 
 
-def browser_init(context, test_name):
+
+def browser_init(context):
     """
     :param context: Behave context
     """
-    # context.driver = webdriver.Chrome(executable_path=r"C:\Users\shekh\OneDrive\Desktop\Vibha\AutoCure\chromedriver_win32\chromedriver.exe")
+     #context.driver = webdriver.Chrome(executable_path=r"C:\Users\shekh\OneDrive\Desktop\Vibha\AutoCure\chromedriver_win32\chromedriver.exe")
     # For chrome
-    # service = Service(r'C:\Users\shekh\OneDrive\Desktop\Vibha\AutoCure\chromedriver_win32\chromedriver.exe')
+    service = Service(r'C:\Users\shekh\OneDrive\Desktop\Vibha\AutoCure\chromedriver_win32\chromedriver.exe')
     # For Firefox
     #service = Service(r'C:\Users\shekh\OneDrive\Desktop\Vibha\AutoCure\geckodriver-v0.33.0-win32.geckodriver.exe')
     ## HEADLESS MODE ####
@@ -24,40 +27,42 @@ def browser_init(context, test_name):
     #     service=service
     # )
 
-    # context.driver = webdriver.Chrome(service=service)
+    context.driver = webdriver.Chrome(service=service)
     # context.driver = webdriver.Safari()
     # context.driver = webdriver.Firefox(service=service)
 
-    # context.driver.maximize_window()
-    # context.driver.implicitly_wait(4)
-    # context.driver.wait = WebDriverWait(context.driver, 10)
-    # context.app = Application(driver=context.driver)
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(4)
+    context.driver.wait = WebDriverWait(context.driver, 10)
+    context.app = Application(driver=context.driver)
+
+
  # for browerstack ###
     # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-    bs_user = 'vibhagoel_0cU5fF'
-    bs_key = '2qjSUtPuqZkBXpSM3KSc'
-
-    desired_cap = {
-        'browserName': 'Firefox',
-        'bstack:options': {
-            'os': 'Windows',
-            'osVersion': '10',
-            'sessionName': test_name
-        }
-    }
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
-
-    context.driver.maximize_window()
-    context.driver.implicitly_wait(5)
-    context.driver.wait = WebDriverWait(context.driver, 10)
-
-    context.app = Application(context.driver)
+    # bs_user = 'vibhagoel_0cU5fF'
+    # bs_key = '2qjSUtPuqZkBXpSM3KSc'
+    #
+    # desired_cap = {
+    #     'browserName': 'Firefox',
+    #     'bstack:options': {
+    #         'os': 'Windows',
+    #         'osVersion': '10',
+    #         'sessionName': test_name
+    #     }
+    # }
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
+    #
+    # context.driver.maximize_window()
+    # context.driver.implicitly_wait(5)
+    # context.driver.wait = WebDriverWait(context.driver, 10)
+    #
+    # context.app = Application(context.driver)
 
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context, scenario.name)
+    browser_init(context)
 
 
 def before_step(context, step):
